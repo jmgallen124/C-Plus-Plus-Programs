a9a0c6feaf2c78d57d61cc1f70782542d7b9d46e
jmgallen124 <110046629+jmgallen124@users.noreply.github.com>
2022-07-27 22:20:14 -0500

    Update Age0.cpp

--- Age0.cpp
+++ Age0.cpp
@@ -2,13 +2,19 @@
 #include <string>
 using namespace std;
 
+// Function to display age
 void printAge() {
+    
     // Declare age variable
     int age = 38;
+	
+    // Function to output age
     cout << age << ".\n";
 }
 
+// Main function to call function to print age variable
 int main() {
+	
 	// Print age variable
 	cout << "My age is ";
 	printAge();
